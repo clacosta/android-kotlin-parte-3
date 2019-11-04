5ca2fbb4bc9d76b6d564d1007cbe7b3ebd9c3076
Claudio Costa <claudio@imaginaction.com.br>
2019-11-04 12:01:30 -0300

    feet: 04 - 05 - Utilizando membros abstratos

--- app/src/main/java/br/com/alura/financask/ui/dialog/AlteraTransacaoDialog.kt
+++ app/src/main/java/br/com/alura/financask/ui/dialog/AlteraTransacaoDialog.kt
@@ -1,39 +1,52 @@
 package br.com.alura.financask.ui.dialog
 
-import android.app.DatePickerDialog
 import android.content.Context
-import android.support.v7.app.AlertDialog
-import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.EditText
-import android.widget.Spinner
-import android.widget.Toast
 import br.com.alura.financask.R
 import br.com.alura.financask.delegate.TransacaoDelegate
-import br.com.alura.financask.extension.converteParaCalendar
 import br.com.alura.financask.extension.formataParaBrasileiro
 import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
-import kotlinx.android.synthetic.main.form_transacao.view.*
-import java.math.BigDecimal
-import java.util.*
 
 class AlteraTransacaoDialog(viewGroup: ViewGroup,
                             private val context: Context) :
         FormularioTransacaoDialog(context, viewGroup) {
 
+    override val tituloBotaoPositivo: String
+        get() = "Alterar"
+
+    override fun tituloPor(tipo: Tipo): Int {
+        if (tipo == Tipo.RECEITA) {
+            return R.string.altera_receita
+        }
+        return R.string.altera_despesa
+    }
+
     fun chama(transacao: Transacao, transacaoDelegate: TransacaoDelegate) {
         val tipo = transacao.tipo
+        super.chama(tipo, transacaoDelegate)
+        inicializaCampos(transacao)
+    }
 
-        super.chama(tipo , transacaoDelegate)
+    private fun inicializaCampos(transacao: Transacao) {
+        inicializaCampoValor(transacao)
+        inicializaCampoData(transacao)
+        inicializaCampoCategotia(transacao)
+    }
 
-        campoValor.setText(transacao.valor.toString())
-        campoData.setText(transacao.data.formataParaBrasileiro())
+    private fun inicializaCampoCategotia(transacao: Transacao) {
+        val tipo = transacao.tipo
         val categoriasRetornadas = context.resources.getStringArray(categoriasPor(tipo))
         val posicaoCategoria = categoriasRetornadas.indexOf(transacao.categoria)
         campoCategoria.setSelection(posicaoCategoria, true)
+    }
+
+    private fun inicializaCampoData(transacao: Transacao) {
+        campoData.setText(transacao.data.formataParaBrasileiro())
+    }
+
+    private fun inicializaCampoValor(transacao: Transacao) {
+        campoValor.setText(transacao.valor.toString())
     }
 
 }
--- app/src/main/java/br/com/alura/financask/ui/dialog/FormularioTransacaoDialog.kt
+++ app/src/main/java/br/com/alura/financask/ui/dialog/FormularioTransacaoDialog.kt
@@ -18,13 +18,15 @@ import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
 import java.util.*
 
-open class FormularioTransacaoDialog(
+abstract class FormularioTransacaoDialog(
         private val context: Context,
         private val viewGroup: ViewGroup) {
+
     private val viewCriada = criaLayout()
     protected val campoValor = viewCriada.form_transacao_valor
     protected val campoCategoria = viewCriada.form_transacao_categoria
     protected val campoData = viewCriada.form_transacao_data
+    protected abstract val tituloBotaoPositivo: String
 
     fun chama(tipo: Tipo, transacaoDelegate: TransacaoDelegate) {
         configuraCampoData()
@@ -39,7 +41,7 @@ open class FormularioTransacaoDialog(
         AlertDialog.Builder(context)
                 .setTitle(titulo)
                 .setView(viewCriada)
-                .setPositiveButton("Adicionar"
+                .setPositiveButton(tituloBotaoPositivo
                 ) { _, _ ->
                     val valorEmTexto = campoValor.text.toString()
                     val dataEmTexto = campoData.text.toString()
@@ -59,12 +61,7 @@ open class FormularioTransacaoDialog(
                 .show()
     }
 
-    private fun tituloPor(tipo: Tipo): Int {
-        if (tipo == Tipo.RECEITA) {
-            return R.string.adiciona_receita
-        }
-        return R.string.adiciona_despesa
-    }
+    protected abstract fun tituloPor(tipo: Tipo): Int
 
     private fun converteCampoValor(valorEmTexto: String): BigDecimal {
         return try {
