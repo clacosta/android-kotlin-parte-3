fb36c747ddc021358a6048b15830b86dc35684d0
Claudio Costa <claudio@imaginaction.com.br>
2019-11-04 16:30:04 -0300

    feet: 06 - 01 - Removendo transações

--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,7 +2,10 @@ package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.ViewGroup
+import android.widget.AdapterView
 import br.com.alura.financask.R
 import br.com.alura.financask.model.Tipo
 import br.com.alura.financask.model.Transacao
@@ -72,7 +75,25 @@ class ListaTransacoesActivity : AppCompatActivity() {
                 val transacao = transacoes[position]
                 chamaDialogDeAlteracao(transacao, position)
             }
+            setOnCreateContextMenuListener { menu, _, _ ->
+                menu.add(Menu.NONE, 1, Menu.NONE, "Remover")
+            }
         }
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        val idDoMenu = item?.itemId
+        if(idDoMenu == 1){
+            val adapterContextMenuInfo = item.menuInfo as AdapterView.AdapterContextMenuInfo
+            val posicao = adapterContextMenuInfo.position
+            remove(posicao)
+        }
+        return super.onContextItemSelected(item)
+    }
+
+    private fun remove(posicao: Int) {
+        transacoes.removeAt(posicao)
+        atualizaTransacoes()
     }
 
     private fun chamaDialogDeAlteracao(transacao: Transacao, position: Int) {
