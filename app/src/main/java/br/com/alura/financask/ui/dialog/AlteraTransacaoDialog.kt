48e522a997a4d8de4244aa6efa04d3f8b5b8fb10
Claudio Costa <claudio@imaginaction.com.br>
2019-11-04 11:45:34 -0300

    feet: 04 - 03 - Aplicando a herança na classe de alteração

--- app/src/main/java/br/com/alura/financask/ui/dialog/AlteraTransacaoDialog.kt
+++ app/src/main/java/br/com/alura/financask/ui/dialog/AlteraTransacaoDialog.kt
@@ -20,19 +20,15 @@ import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
 import java.util.*
 
-class AlteraTransacaoDialog(private val viewGroup: ViewGroup,
-                            private val context: Context) {
-
-    private val viewCriada: View = criaLayout()
-    private val campoValor: EditText = viewCriada.form_transacao_valor
-    private val campoData: EditText = viewCriada.form_transacao_data
-    private val campoCategoria: Spinner = viewCriada.form_transacao_categoria
+class AlteraTransacaoDialog(viewGroup: ViewGroup,
+                            private val context: Context) :
+        FormularioTransacaoDialog(context, viewGroup) {
 
     fun chama(transacao: Transacao, transacaoDelegate: TransacaoDelegate) {
         val tipo = transacao.tipo
-        configuraCampoData()
-        configuraCampoCategoria(tipo)
-        configuraFormulario(tipo, transacaoDelegate)
+
+        super.chama(tipo , transacaoDelegate)
+
         campoValor.setText(transacao.valor.toString())
         campoData.setText(transacao.data.formataParaBrasileiro())
         val categoriasRetornadas = context.resources.getStringArray(categoriasPor(tipo))
@@ -40,95 +36,4 @@ class AlteraTransacaoDialog(private val viewGroup: ViewGroup,
         campoCategoria.setSelection(posicaoCategoria, true)
     }
 
-    private fun configuraFormulario(tipo: Tipo, transacaoDelegate: TransacaoDelegate) {
-
-        val titulo = tituloPor(tipo)
-
-        AlertDialog.Builder(context)
-                .setTitle(titulo)
-                .setView(viewCriada)
-                .setPositiveButton("Alterar"
-                ) { _, _ ->
-                    val valorEmTexto = campoValor.text.toString()
-                    val dataEmTexto = campoData.text.toString()
-                    val categoriaEmTexto = campoCategoria.selectedItem.toString()
-
-                    val valor = converteCampoValor(valorEmTexto)
-                    val data = dataEmTexto.converteParaCalendar()
-
-                    val transacaoCriada = Transacao(tipo = tipo,
-                            valor = valor,
-                            data = data,
-                            categoria = categoriaEmTexto)
-
-                    transacaoDelegate.delegate(transacaoCriada)
-                }
-                .setNegativeButton("Cancelar", null)
-                .show()
-    }
-
-    private fun tituloPor(tipo: Tipo): Int {
-        if (tipo == Tipo.RECEITA) {
-            return R.string.altera_receita
-        }
-        return R.string.altera_despesa
-    }
-
-    private fun converteCampoValor(valorEmTexto: String): BigDecimal {
-        return try {
-            BigDecimal(valorEmTexto)
-        } catch (exception: NumberFormatException) {
-            Toast.makeText(context,
-                    "Falha na conversão de valor",
-                    Toast.LENGTH_LONG)
-                    .show()
-            BigDecimal.ZERO
-        }
-    }
-
-    private fun configuraCampoCategoria(tipo: Tipo) {
-
-        val categorias = categoriasPor(tipo)
-
-        val adapter = ArrayAdapter
-                .createFromResource(context,
-                        categorias,
-                        android.R.layout.simple_spinner_dropdown_item)
-
-        campoCategoria.adapter = adapter
-    }
-
-    private fun categoriasPor(tipo: Tipo): Int {
-        if (tipo == Tipo.RECEITA) {
-            return R.array.categorias_de_receita
-        }
-        return R.array.categorias_de_despesa
-    }
-
-    private fun configuraCampoData() {
-        val hoje = Calendar.getInstance()
-
-        val ano = hoje.get(Calendar.YEAR)
-        val mes = hoje.get(Calendar.MONTH)
-        val dia = hoje.get(Calendar.DAY_OF_MONTH)
-
-        campoData.setText(hoje.formataParaBrasileiro())
-        campoData.setOnClickListener {
-            DatePickerDialog(context,
-                    { _, ano, mes, dia ->
-                        val dataSelecionada = Calendar.getInstance()
-                        dataSelecionada.set(ano, mes, dia)
-                        campoData.setText(dataSelecionada.formataParaBrasileiro())
-                    }
-                    , ano, mes, dia)
-                    .show()
-        }
-    }
-
-    private fun criaLayout(): View {
-        return LayoutInflater.from(context)
-                .inflate(R.layout.form_transacao,
-                        viewGroup,
-                        false)
-    }
 }
